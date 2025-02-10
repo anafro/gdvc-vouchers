16e3bdac1d57597467e0fead33245eb2c76486a7
Anatoly Frolov <44891639+anafro@users.noreply.github.com>
2025-02-10 15:25:25 +1000

    feat(ui): show certificates as a list

--- main.py
+++ main.py
@@ -3,8 +3,29 @@ from screeninfo import get_monitors
 
 
 MONITOR = get_monitors().pop()
-WINDOW_SIZE = (800, 500)
+WINDOW_SIZE = (MONITOR.width, MONITOR.height)
 WINDOW_POSITION = (int(MONITOR.width / 2 - WINDOW_SIZE[0] / 2), int(MONITOR.height / 2 - WINDOW_SIZE[1] / 2))
+
+@eel.expose
+def get_certificates() -> list[dict[str, str]]:
+    return [
+        {
+            "id": 123456,
+            "holder": "Иванов Иван Иванович",
+            "phone": "+7 999 888 11 22",
+            "service": "Химчистка ковров",
+            "value": "9 900 рублей",
+            "expire": "20 мая 2025",
+        },
+        {
+            "id": 123456,
+            "holder": "Иванов Иван Иванович",
+            "phone": "+7 999 888 11 22",
+            "service": "Химчистка ковров",
+            "value": "9 930 рублей",
+            "expire": "20 мая 2025",
+        },
+    ]
 
 def main() -> None:
     eel.init('web')
--- web/main.js
+++ web/main.js
@@ -1,3 +1,3 @@
-(() => {
-
-})();
+function getCertificates() {
+    return eel["get_certificates"]();
+}
