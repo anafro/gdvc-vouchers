69f1ba432fb06583c6b255eae7701e2470de8772
Anatoly Frolov <44891639+anafro@users.noreply.github.com>
2025-02-24 12:33:01 +1000

    fix: kill server when app is closed

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import sys
+
 import eel
 from screeninfo import get_monitors
+import os
 
 from models.certificate import Certificate, initialize_db
 
@@ -7,6 +10,16 @@ from models.certificate import Certificate, initialize_db
 MONITOR = get_monitors().pop()
 WINDOW_SIZE = (MONITOR.width, MONITOR.height)
 WINDOW_POSITION = (int(MONITOR.width / 2 - WINDOW_SIZE[0] / 2), int(MONITOR.height / 2 - WINDOW_SIZE[1] / 2))
+
+
+def get_web_folder():
+    if hasattr(sys, '_MEIPASS'):
+        # If running as a packaged exe, the path to the bundled web folder
+        return os.path.join(sys._MEIPASS, 'web')
+    else:
+        # If running from source, use the local web folder
+        return os.path.join(os.path.dirname(__file__), 'web')
+
 
 @eel.expose
 def get_certificates() -> list[dict[str, str]]:
@@ -28,10 +41,27 @@ def delete_certificate(id: int) -> None:
     Certificate.delete_certificate_by_id(id)
 
 
+@eel.expose
+def close_python(*args):
+    sys.exit()
+
+
+def close(page, sockets_still_open):
+    eel._stop()
+    os._exit(0)
+
+
 def main() -> None:
     initialize_db()
-    eel.init('web')
-    eel.start('index.html', size=WINDOW_SIZE, position=WINDOW_POSITION)
+    eel.init(get_web_folder())
+    eel.start('index.html', size=WINDOW_SIZE, position=WINDOW_POSITION, close_callback=close)
+
+    try:
+        while True:
+            eel.sleep(10)
+    except KeyboardInterrupt:
+        print("Shutting down gracefully")
+        eel.close()
 
 
 if __name__ == '__main__':
--- web/main.js
+++ web/main.js
@@ -1,3 +1,5 @@
+window.addEventListener("beforeunload", () => { eel.close_python()} )
+
 function app() {
     return {
         certificates: [],
@@ -58,4 +60,92 @@ async function deleteCertificate(id) {
     }
 
     await eel["delete_certificate"](parseInt(id))();
+}
+
+async function loadFonts() {
+    const fonts = [
+        {
+            name: 'Zvezda',
+            filename: 'zvezda.ttf',
+        },
+        {
+            name: 'Montserrat',
+            filename: 'montserrat.ttf',
+        },
+    ];
+
+    const fontPromises = fonts.map(font => {
+        const fontFace = new FontFace(font.name, `url(/fonts/${font.filename})`);
+        document.fonts.add(fontFace);
+        return fontFace.load();
+    });
+
+    await Promise.all(fontPromises);
+}
+
+function loadImage(src) {
+    return new Promise((resolve, reject) => {
+        const image = new Image();
+        image.onload = () => resolve(image);
+        image.onerror = (error) => reject('Image failed to load: ' + error);
+        image.src = src;
+    });
+}
+
+async function generateCertificateImage(certificate) {
+    const cyan = '#00D5BA';
+    const black = '#000000';
+
+    try {
+        await loadFonts();
+        const template = await loadImage('images/certificate.png');
+
+        // Check if image loaded correctly
+        if (!template.width || !template.height) {
+            console.error("Image not loaded correctly.");
+            return;
+        }
+
+        const canvas = document.createElement('canvas');
+        const context = canvas.getContext('2d');
+        canvas.width = template.width;
+        canvas.height = template.height;
+
+        context.drawImage(template, 0, 0, canvas.width, canvas.height);
+
+        context.fillStyle = black;
+        context.font = `1000 32px Montserrat`;
+        context.fillText(`#${certificate.id}`, 988, 105);
+
+        context.fillStyle = cyan;
+        context.font = `280px Zvezda`;
+        context.fillText(certificate.value, 978, 526);
+
+        context.fillStyle = cyan;
+        context.font = `84px Zvezda`;
+        context.fillText('рублей', 978, 623);
+
+        context.fillStyle = cyan;
+        context.font = `bold 48px Montserrat`;
+        context.fillText(`На услугу "${certificate.service}"`, 978, 693);
+
+        context.fillStyle = black;
+        context.font = `bold 24px Montserrat`;
+        context.fillText(`Этот сертификат действителен до ${new Date(certificate.expires).toLocaleDateString('ru-RU', {
+            day: 'numeric',
+            month: 'long',
+            year: 'numeric'
+        })}. Поторопитесь!`, 978, 891);
+
+        context.fillStyle = '#B3B3B3';
+        context.font = `bold 24px Montserrat`;
+        context.fillText(`Владелец: ${certificate.holder}`, 978, 960);
+
+        const image = document.getElementById(`certificates__card-options-certificate-${certificate.id}`);
+        const base64 = canvas.toDataURL("image/png");
+
+        image.src = base64;
+    } catch (error) {
+        console.error("Error generating certificate image:", error);
+    }
 }
